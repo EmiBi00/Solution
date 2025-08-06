84efe4f46bc9f437803cdb269e9741185b0c654d
Emi <emmi.bielmeier@blaibach.eu>
2025-08-06 15:52:04 +0200

    completed busyStudent

--- src/Solution.java
+++ src/Solution.java
@@ -16,4 +16,15 @@ public class Solution {
         }
         return square;
     }
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int output = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                output++;
+            }
+        }
+        return output;
+    }
+
 }
