df7f521fc554d46668159648760ce6fd3cba417d
Emi <emmi.bielmeier@blaibach.eu>
2025-08-07 15:13:56 +0200

    completed distributeCandies

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,8 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
 
     public int[] sortedSquares(int[] nums) {
@@ -27,4 +32,49 @@ public class Solution {
         return output;
     }
 
+    static class Interval {
+
+        private final int begin;
+        private final int ende;
+
+        public Interval(int begin, int ende) {
+            this.begin = begin;
+            this.ende = ende;
+        }
+
+        public int getBegin() {
+            return begin;
+        }
+
+        public int getEnde() {
+            return ende;
+        }
+    }
+
+    public int busyStudent(Interval[] intervals, int queryTime) {
+        int output = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i].getBegin() >= queryTime && intervals[i].getEnde() <= queryTime) {
+                output++;
+            }
+        }
+        return output;
+    }
+
+    public int busyStudent(List<int[]> times, int queryTime) {
+        int output = 0;
+        for (int[] time : times) {
+            if (time[0] >= queryTime && time[1] <= queryTime) {
+                output++;
+            }
+        }
+        return output;
+    }
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> ordner = new HashSet<>();
+        for (int type : candyType) {
+            ordner.add(type);
+        }
+        return Math.min(ordner.size(), (candyType.length / 2));
+    }
 }
