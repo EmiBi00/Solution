98d1258d469976aafb7dd9c2c9f8237d31d102a7
Emi <emmi.bielmeier@blaibach.eu>
2025-08-23 07:38:14 +0200

    completed evaluateTree

--- src/Solution.java
+++ src/Solution.java
@@ -312,4 +312,23 @@ public class Solution {
         }
         return -1;
     }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public boolean evaluateTree(TreeNode root) {
+        if (root.val == 0 || root.val == 1) { // Basisfall: Ende der Rekursion, muss einen Wert returnen
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        if (root.val == 3) {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+        return false;
+    }
 }
