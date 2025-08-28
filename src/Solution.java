a377e4908e381474a3ba9f74f282d56d3c362c6c
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-08-28 15:53:34 +0200

    completed isMonotonic

--- src/Solution.java
+++ src/Solution.java
@@ -349,4 +349,19 @@ public class Solution {
         }
         return null;
     }
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            }
+            if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
 }
