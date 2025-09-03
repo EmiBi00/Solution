aa6d73d82f9611a633b1055fa6cc5fbe5f091298
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-09-03 19:22:02 +0200

    completed recoverOrder

--- src/Solution.java
+++ src/Solution.java
@@ -472,4 +472,18 @@ public class Solution {
         }
         return output;
     }
+    public int[] recoverOrder(int[] order, int[] friends) {
+        int [] output = new int[friends.length];
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < friends.length; i++) {
+            set.add(friends[i]);
+        }
+        int index = 0;
+        for(int i = 0; i < order.length; i++) {
+            if (set.contains(order[i])) {
+                output[index++] = order[i];
+            }
+        }
+        return output;
+    }
 }
