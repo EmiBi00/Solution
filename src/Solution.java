810112961a624449a012d114705568c0cb9a188e
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-09-25 18:17:31 +0200

    completed minimumSum

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,6 @@
 import java.beans.PropertyEditorSupport;
 import java.util.*;
+import java.util.Arrays;
 
 public class Solution {
 
@@ -535,5 +536,18 @@ public class Solution {
             }
         }
         return ans;
+    }
+    public int minimumSum(int num) {
+        int[] digits = new int[4];
+        for (int i = 0; i < 4; i++) {
+            digits[i] = num % 10;
+            num /= 10;
+        }
+        Arrays.sort(digits);
+
+        int new1 = digits[0] * 10 + digits[2];
+        int new2 = digits[1] * 10 + digits[3];
+
+        return new1 + new2;
     }
 }
