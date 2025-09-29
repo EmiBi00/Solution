bc327cef129a748f99d15c236ffca3e567be2b69
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-09-29 15:11:34 +0200

    completed matrixReshape

--- src/Solution.java
+++ src/Solution.java
@@ -605,4 +605,23 @@ public class Solution {
         return true;
         // return !s.contains("LLL") && s.indexOf("A") == s.lastIndexOf("A");
     }
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if (mat.length * mat[0].length != c * r) {
+            return mat;
+        }
+        int[][] reshapeM = new int[r][c];
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                reshapeM[row][col] = mat[i][j];
+                col++;
+                if (col >= c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        return reshapeM;
+    }
 }
