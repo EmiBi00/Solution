25ced0cfa673cd54b4b8f0998a6981ef0d13da72
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-10-05 13:44:41 +0200

    completed alternatingSum

--- src/Solution.java
+++ src/Solution.java
@@ -701,4 +701,15 @@ public class Solution {
         }
         return s;
     }
+    public int alternatingSum(int[] nums) {
+        int output = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                output += nums[i];
+            } else {
+                output -= nums[i];
+            }
+        }
+        return output;
+    }
 }
