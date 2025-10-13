fb2120abfa45ce5ec089d76ada4d1a9e1c164d33
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-10-13 20:14:38 +0200

    completed canPlaceFlowers

--- src/Solution.java
+++ src/Solution.java
@@ -787,7 +787,22 @@ public class Solution {
         }
         return output;
     }
-
-
-
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            boolean left = false;
+            boolean right = false;
+            if (i == 0 || flowerbed[i - 1] == 0) {
+                left = true;
+            }
+            if (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) {
+                right = true;
+            }
+            if (left && right && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                count++;
+            }
+        }
+        return count >= n;
+    }
 }
