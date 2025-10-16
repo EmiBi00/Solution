bb0798281ec2540fb7cf9d1883726c7539812871
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-10-16 17:58:14 +0200

    completed earliestTime

--- src/Solution.java
+++ src/Solution.java
@@ -818,4 +818,17 @@ public class Solution {
         }
         return output;
     }
+    public int earliestTime(int[][] tasks) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < tasks.length; i++) {
+            int current = 0;
+            for (int j = 0; j < tasks[i].length; j++) {
+               current += tasks[i][j];
+            }
+            if (current < min) {
+                min = current;
+            }
+        }
+        return min;
+    }
 }
