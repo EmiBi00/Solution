90d1f5dfb69c9aa9377b1eceb66f4414eb0ed495
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-10-19 17:35:02 +0200

    completed isArraySpecial

--- src/Solution.java
+++ src/Solution.java
@@ -849,4 +849,12 @@ public class Solution {
         }
         return output;
     }
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == nums[i +1] % 2) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
