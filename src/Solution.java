fd65717a888ae0ae4340b27dd499aca499ce76cf
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-11-05 20:51:30 +0100

    completed plusMinus

--- src/Solution.java
+++ src/Solution.java
@@ -991,5 +991,22 @@ public class Solution {
         }
         return output;
     }
+    public static void plusMinus(List<Integer> arr) {
+        double positiv = 0;
+        double negativ = 0;
+        double zero = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == 0) {
+                zero++;
+            }else if (arr.get(i) < 0) {
+                negativ++;
+            }else {
+                positiv++;
+            }
+        }
+        System.out.println(positiv/arr.size());
+        System.out.println(negativ/arr.size());
+        System.out.println(zero/arr.size());
+    }
 
 }
