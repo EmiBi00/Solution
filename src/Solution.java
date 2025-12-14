5273b728bb51cdf92c731473f3231571dc618e3d
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-12-14 20:16:41 +0100

    completed libraryFine

--- src/Solution.java
+++ src/Solution.java
@@ -1320,4 +1320,18 @@ public class Solution {
         }
         return Arrays.asList(maxCount, minCount);
     }
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        int output = 0;
+        if (y1 > y2) {
+            output += 10000;
+        }
+        if (y1 == y2 && m1 > m2) {
+            output += 500 * (m1 - m2);
+        }
+        if (y1 == y2 && m1 == m2 && d1 > d2) {
+            output += 15 * (d1 - d2);
+        }
+        return output;
+    }
+
 }
