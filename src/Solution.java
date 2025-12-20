cfc2486e69c8c93d0b2d5bd513b426dda731b54d
EmiBi00 <emmi.bielmeier@blaibach.eu>
2025-12-20 16:30:59 +0100

    completed getTotalX

--- src/Solution.java
+++ src/Solution.java
@@ -1334,4 +1334,38 @@ public class Solution {
         return output;
     }
 
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        int lcmA = a.get(0);
+        for (int i = 1; i < a.size(); i++) {
+            lcmA = lcm(lcmA, a.get(i));
+        }
+
+        int gcdB = b.get(0);
+        for (int i = 1; i < b.size(); i++) {
+            gcdB = gcd(gcdB, b.get(i));
+        }
+
+        int count = 0;
+        for (int multiple = lcmA; multiple <= gcdB; multiple += lcmA) {
+            if (gcdB % multiple == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private static int lcm(int a, int b) {
+        return a * (b / gcd(a, b));
+    }
+
 }
