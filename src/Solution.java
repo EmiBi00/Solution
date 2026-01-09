ab0f61ee6526de2d9321f15320a249203fc8e55b
EmiBi00 <emmi.bielmeier@blaibach.eu>
2026-01-09 21:25:35 +0100

    completed maxWidthOfVerticalArea

--- src/Solution.java
+++ src/Solution.java
@@ -1486,5 +1486,19 @@ public class Solution {
         String rest = s.substring(k);
         return reversedPrefix + rest;
     }
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int n = points.length;
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = points[i][0];
+        }
+        Arrays.sort(x);
+        int maxWidth = 0;
+        for (int i = 1; i < n; i++) {
+            maxWidth = Math.max(maxWidth, x[i] - x[i - 1]);
+        }
+
+        return maxWidth;
+    }
 
 }
