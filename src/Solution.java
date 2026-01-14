04f68e9c7cdbbf8e88e94decdfee78ee69eebf8c
EmiBi00 <emmi.bielmeier@blaibach.eu>
2026-01-14 19:29:35 +0100

    completed getFinalState

--- src/Solution.java
+++ src/Solution.java
@@ -1510,5 +1510,17 @@ public class Solution {
         }
         return Math.abs(n - reversed);
     }
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i = 0; i < k; i++) {
+            int minIndex = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
 
 }
