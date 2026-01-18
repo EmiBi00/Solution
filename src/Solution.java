2a8205db716d7b5391c71beeb5d42b03e2e2591f
EmiBi00 <emmi.bielmeier@blaibach.eu>
2026-01-18 05:39:40 +0100

    completed numOfStrings

--- src/Solution.java
+++ src/Solution.java
@@ -1522,5 +1522,14 @@ public class Solution {
         }
         return nums;
     }
+    public int numOfStrings(String[] patterns, String word) {
+        int output = 0;
+        for (String pattern : patterns) {
+            if (word.contains(pattern)) {
+                output++;
+            }
+        }
+        return output;
+    }
 
 }
