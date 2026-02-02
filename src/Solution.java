5864608de2108eadc2850f6e6ec6eaad4b90b602
EmiBi00 <emmi.bielmeier@blaibach.eu>
2026-02-02 20:32:51 +0100

    completed subarraySum

--- src/Solution.java
+++ src/Solution.java
@@ -1531,5 +1531,14 @@ public class Solution {
         }
         return output;
     }
+    public int subarraySum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = Math.max(0, i - nums[i]); j <= i; j++) {
+                sum += nums[j];
+            }
+        }
+        return sum;
+    }
 
 }
